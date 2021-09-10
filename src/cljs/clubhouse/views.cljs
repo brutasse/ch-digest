3b3e3867289c8ecdad0e0368a2e159e3422335ca
Bruno Renié <brutasse@users.noreply.github.com>
2021-09-10 14:36:32 +0200

    view: remove state changes, keep only comments

--- src/cljs/clubhouse/views.cljs
+++ src/cljs/clubhouse/views.cljs
@@ -8,90 +8,28 @@
     [cljsjs.moment]
     [markdown.core :refer [md->html]]))
 
-(def story-color
-  {"chore"   "blue"
-   "feature" "yellow"
-   "bug"     "red"})
-
 (defn header-menu
   []
   (let [{:keys [day next? previous?]} @(re-frame/subscribe [::subs/day])]
     [sa/Menu {:inverted true}
      [sa/Container
-      [sa/MenuItem "ClubHouse Activity"]
+      [sa/MenuItem "Shortcut Conversations"]
       [sa/MenuItem
-      [sa/ButtonGroup {:size :tiny}
-      [sa/Button {:disabled (not previous?)
-                  :size :tiny
-                  :on-click #(re-frame/dispatch [::events/change-day -1])}
-       "prev"]
-      [sa/Button {:size :tiny :color :teal} (str day)]
-      [sa/Button {:disabled (not next?)
-                  :size :tiny
-                  :on-click #(re-frame/dispatch [::events/change-day 1])}
-       "next"]]]]]))
-
-
-(def prev-col-counts
-  {"Unscheduled" 0
-   "Ready for Development" 0
-   "In Development" 1
-   "In progress" 1
-   "Ready for Review" 2
-   "Ready for Deploy" 3
-   "Completed" 4})
-
-(defn story-state
-  [workflow-states [_ {:keys [old-state state]}]]
-  (+
-   (* 10 (prev-col-counts (:name (get workflow-states state))))
-   (prev-col-counts (:name (get workflow-states (or old-state state))))))
+       [sa/ButtonGroup {:size :tiny}
+        [sa/Button {:disabled (not previous?)
+                    :size :tiny
+                    :on-click #(re-frame/dispatch [::events/change-day -1])}
+         "prev"]
+        [sa/Button {:size :tiny :color :teal} (str day)]
+        [sa/Button {:disabled (not next?)
+                    :size :tiny
+                    :on-click #(re-frame/dispatch [::events/change-day 1])}
+         "next"]]]]]))
 
 (defn project-section
   [project stories metadata]
   [:<>
    [sa/Header {:as "h2"} (:name project)]
-   [sa/Grid {:columns 5}
-    [sa/GridRow {:stretched true}
-     [sa/GridColumn [:strong "New"]]
-     [sa/GridColumn [:strong "In Progress"]]
-     [sa/GridColumn [:strong "Ready for review"]]
-     [sa/GridColumn [:strong "Ready for deploy"]]
-     [sa/GridColumn [:strong "Completed"]]]
-    (for [[id story] (sort-by (partial story-state (:workflow-states metadata))
-                              stories)
-          :let [full-story     (get-in metadata
-                                       [:stories (-> id str keyword)])
-                workflow-state (get-in metadata
-                                       [:workflow-states (:state story)])
-                old-state      (get-in metadata
-                                       [:workflow-states (:old-state story)])]]
-      ^{:key id}
-      [sa/GridRow {:stretched true}
-       (if old-state
-         (let [prev (get prev-col-counts (:name old-state))
-               tot (get prev-col-counts (:name workflow-state))
-               gap (- tot 1 prev)]
-           [:<>
-            (for [i (range prev)]
-              ^{:key i} [sa/GridColumn])
-            (when (not= prev tot)
-              ^{:key "card"}
-              [sa/GridColumn [sa/Card {:style {:opacity 0.4}
-                                       :description (:name full-story)}]])
-            (for [i (range gap)]
-              ^{:key i} [sa/GridColumn {:verticalAlign :middle}
-                         [:div {:style {:text-align :center}}
-                     [sa/Icon {:name "right arrow"
-                               :size :large
-                               :style {:opacity 0.2}}]]])])
-         (for [i (range (get prev-col-counts (:name workflow-state)))]
-           ^{:key i} [sa/GridColumn ""]))
-       [sa/GridColumn [sa/Card {:href (:app_url full-story)
-                                :target :_blank
-                                :color (story-color (:story_type full-story))
-                                :description (:name full-story)}]]])]
-
    (for [[id story] stories
          :when (:comments story)
          :let [full-story (get-in metadata [:stories (-> id str keyword)])]]
@@ -108,8 +46,8 @@
       (for [comm (:comments story)
             :let [full-comm (get-in metadata [:comments comm])
                   author    (get-in metadata [:members (-> full-comm
-                                                         :author_id
-                                                         keyword)])
+                                                           :author_id
+                                                           keyword)])
                   gravatar  (str "https://www.gravatar.com/avatar/"
                                  (-> author :profile :gravatar_hash)
                                  ".jpg?s=48&d=identicon")
@@ -134,17 +72,17 @@
   []
   (let [state    @(re-frame/subscribe [::subs/state])
         metadata (-> [::subs/meta]
-                   re-frame/subscribe
-                   deref
-                   (assoc :workflow-states (:workflow-states state)
-                          :comments (:comments state)))]
+                     re-frame/subscribe
+                     deref
+                     (assoc :workflow-states (:workflow-states state)
+                            :comments (:comments state)))]
     [sa/Container
-    (for [[id stories] (sort-by (partial project-name metadata)
-                                (:projects state))
-          :let [project (get-in metadata [:projects (-> id str keyword)])]
-          :when project]
-      ^{:key (str "project-" id)}
-      [project-section project stories metadata])
+     (for [[id stories] (sort-by (partial project-name metadata)
+                                 (:projects state))
+           :let [project (get-in metadata [:projects (-> id str keyword)])]
+           :when project]
+       ^{:key (str "project-" id)}
+       [project-section project stories metadata])
      (when-not (:projects state)
        "No data.")]))
 
